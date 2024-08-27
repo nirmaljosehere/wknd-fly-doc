66d20e4e1dd4501f8a6c831185ce1944a8713e8c
Navdeep Singh <navdeeps@adobe.com>
2024-08-27 18:17:11 +0530

    fix blocks

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -4,8 +4,6 @@
  * https://www.hlx.live/developer/block-collection/accordion
  */
 
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
   [...block.children].forEach((row) => {
     // decorate accordion item label
@@ -18,7 +16,6 @@ export default function decorate(block) {
     body.className = 'accordion-item-body';
     // decorate accordion item
     const details = document.createElement('details');
-    moveInstrumentation(row, details);
     details.className = 'accordion-item';
     details.append(summary, body);
     row.replaceWith(details);
